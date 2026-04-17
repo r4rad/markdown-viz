c8b571bff3f341238ed5b127fd4e91d86aad20fe
r4rad <rad.rafatahmad@gmail.com>
2026-04-17 14:08:01 +0600

    chore: add VITE_ENABLE_SHARING env var (default true)

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,6 +7,13 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_STORAGE_BUCKET: string;
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly VITE_FIREBASE_APP_ID: string;
+  readonly VITE_MAX_SYNC_TABS: string;
+  readonly VITE_AUTO_SYNC_INTERVAL_SECONDS: string;
+  readonly VITE_ENABLE_SHARING: string;
+  readonly VITE_EMAILJS_SERVICE_ID: string;
+  readonly VITE_EMAILJS_TEMPLATE_ID: string;
+  readonly VITE_EMAILJS_PUBLIC_KEY: string;
+  readonly VITE_FEEDBACK_EMAIL: string;
 }
 
 interface ImportMeta {
